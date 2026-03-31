938477e45211d1d4366cd5b49adb3fe9bc68b6f1
Leo Rouleau <leorouleau5070@gmail.com>
2026-03-30 22:11:43 -0400

    printf done

--- kernel.c
+++ kernel.c
@@ -1,4 +1,5 @@
 #include "kernel.h"
+#include "common.h"
 
 extern char __bss[], __bss_end[], __stack_top[];
 
@@ -26,6 +27,8 @@ void putchar(char ch) {
 }
 
 void kernel_main(void) {
+	printf("\nHello %s \n", "World!");
+    printf("1 + 2 = %d, %x\n", 1 + 2, 0x1234abcd);
 	const char* s = "\n\nHello World!\n";
 	for (int i = 0; s[i] != '\0'; i++) {
 		putchar(s[i]);
