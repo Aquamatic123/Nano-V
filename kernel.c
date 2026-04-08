47c7285ef0d083bb83c25ed7c0f6b4a19c3bb202
Leo Rouleau <leorouleau5070@gmail.com>
2026-04-07 22:51:43 -0400

    testing memory allocation

--- kernel.c
+++ kernel.c
@@ -1,4 +1,4 @@
-include "kernel.h"
+#include "kernel.h"
 #include "common.h"
 
 extern char __bss[], __bss_end[], __stack_top[];
@@ -128,6 +128,11 @@ void kernel_entry(void) {
 
 void kernel_main(void) {
 	memset(__bss, 0, (size_t) __bss_end - (size_t) __bss);
+
+    paddr_t paddr0 = alloc_pages(2);
+    paddr_t paddr1 = alloc_pages(1);
+    printf("alloc_pages test: paddr0=%x\n", paddr0);
+    printf("alloc_pages test: paddr1=%x\n", paddr1);
 
 	printf("\nHello %s \n", "World!");
     printf("1 + 2 = %d, %x\n", 1 + 2, 0x1234abcd);
